08fce1e053ad89b5810e3fe274f52d4532893626
Yuvanthi2003 <142141258+Yuvanthi2003@users.noreply.github.com>
2025-12-11 13:53:33 +0530

    Correct build.sh location and content

--- durgotsav_project/build.sh
+++ durgotsav_project/build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Install dependencies
 pip install -r requirements.txt
+
+# Run migrations
 python manage.py migrate --noinput
+
+# Collect static files
 python manage.py collectstatic --noinput
